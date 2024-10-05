f8310dac88f56fa7af7f071ed2f3e924a95b61f3
Philipp Schmitz <schmxtz@gmail.com>
2024-10-05 15:49:29 +0800

    First word pairings and updated code

--- transcription.py
+++ transcription.py
@@ -1,43 +1,70 @@
 from gruut import sentences
-
-text = ('Abend Auto')
+import pandas
 
 mappings = {
+    # Diphtongs
     'aʊ̯': '\u30A2\u30AA',
-    'toː': '\u30C8',
+
+    # Long Vowels
     'aː': '\u30A2',
+
+    # B+Vowel (Long Vowel)
     'bə': '\u30D9',
-    't': '\u30C8',
-    'n': '\u30F3',
     'ba': '\u30D0',
+    'biː': '\u30D3',
+
+    # D+Vowel
+    'dɐ': '\u30C0\u30FC',
+
+    # L+Vowel (Long Vowel)
+    'luː': '\u30EB\u30FC',
+
+    # M+Vowel (Long Vowel)
+    'mə': '\u30E1',
+
+    # R+Vowel (Long Vowel)
+    'ʁiː': '\u30EA\u30FC',
+    'ʁoː': '\u30ED\u30FC',
+    'ʁuː': '\u30EB\u30FC',
+
+    # T+Vowel
+    'toː': '\u30C8',
+
+    # Consonants
+    'n': '\u30F3',
     'ŋ': '\u30F3',
     'k': '\u30AF',
-    'biː': '\u30D3',
-    'ʁ': '\u30A2',
-    'bluː': '\u30D6\u30EB\u30FC',
-    'mə': '\u30E1'
+    't': '\u30C8',
+    'ʁ': '\u30A2',  # R-sound
+    'b': '\u30D6',
+    'f': '\u30D5',
+
+    # Full syllables (Need seperation because 'x' sounds depends on previous one)
+    'buːx': '\u30D6\u30FC\u30D5'
+
 }
 
+
 def convert_katakana(text):
-    for sent in sentences(text, lang="de-de"):
+    converted_words = []
+    for sent in sentences(text, lang='de-de'):
         for word in sent:
             if word.phonemes:
-                word_ipa = "".join(word.phonemes)
+                word_ipa = ''.join(word.phonemes)
                 word_katakana = []
                 start = 0
                 while start < len(word_ipa):
-                    for i in range(min(len(word_ipa)-start, 4), 0, -1):
-                        if word_ipa[start:start+i] in mappings:
-                            word_katakana.append(mappings[word_ipa[start:start+i]])
+                    modified = False
+                    for i in range(min(len(word_ipa) - start, 4), 0, -1):
+                        if word_ipa[start:start + i] in mappings:
+                            word_katakana.append(mappings[word_ipa[start:start + i]])
                             start += i
+                            modified = True
                             break
-                print("".join(word_katakana))
+                    if not modified:
+                        print('Error converting {}'.format(word_ipa))
+                        break
+                converted_words.append(''.join(word_katakana))
+    return converted_words
 
-
-for sent in sentences("Blume", lang="de-de"):
-    for word in sent:
-        if word.phonemes:
-            print(word.phonemes)
-
-convert_katakana("Blume")
-
+word_pairings = pandas.read_csv('words.csv')
