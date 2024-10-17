7257397bda05f8dc91c45e211df9a57481fdb88e
Philipp Schmitz <schmxtz@gmail.com>
2024-10-17 13:09:23 +0800

    New mappings

--- example.py
+++ example.py
@@ -6,18 +6,18 @@ from transcription.katakanizer import Katakanizer
 kata = Katakanizer('lang-de')
 kata.init_katakanizer()
 
-for entry in kata.phonetics_transcriber.lookup_table:
-    ipa = kata.phonetics_transcriber.lookup_word(entry)
-    # if ipa.startswith('\u0000' + 'ˈviː'):
-    if 'kaːhaːɛl' in ipa:
-        print(entry, ipa)   
+# for entry in kata.phonetics_transcriber.lookup_table:
+#     ipa, t = kata.phonetics_transcriber.lookup_word(entry)
+#     if ipa.startswith('\u0000' + 'xə'):
+#     # if 'hʏ' in ipa:
+#         print(entry, ipa)   
 
 # Making sure old words don't break
 word_pairings = pandas.read_csv(filepath_or_buffer='words.csv')
 for index, row in word_pairings.iterrows():
     converted_word, word_ipa = kata.transcribe_word(row['german'])
     if converted_word != row['katakana']:
-        print('"""""""""""""""')
+        print(row['katakana'], converted_word, '"""""""""""""""')
 
 # New sounds
 text = open('text.txt', encoding='utf-8').readline()
