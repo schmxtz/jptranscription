71408629fff57e4f02417c7ebe9a4f0e2d0c75b4
Philipp Schmitz <schmxtz@gmail.com>
2024-10-29 12:32:23 +0800

    Restructure Phonetizer

--- jptranscription/phonetics/phonetizer.py
+++ jptranscription/phonetics/phonetizer.py
@@ -8,63 +8,70 @@ SUPPORTED_LANGS = {
 
 
 class IPATranscription:
-    def __init__(self, lang: str = 'lang-de'):
+    def _init_(self, lang: str = 'lang-de'):
         self.lang = lang
         self.lookup_table = None
-        if SUPPORTED_LANGS.get(self.lang) is None:
-            raise Exception('The given language {} does is not supported. List of supported languages are {}.'
-                            .format(self.lang, list(SUPPORTED_LANGS.keys())))
+        assert(SUPPORTED_LANGS.get(self.lang) is not None)
+        # Init lookup table
         file_path = Path(__file__).parent.joinpath(SUPPORTED_LANGS[self.lang])
         file_handle = open(str(file_path))
         self.lookup_table = json.load(file_handle)     
 
     def lookup_word(self, word):
-        if not self.lookup_table:
-            raise Exception('Lookup table empty. Initialize it first with init_lookup_table.')
         ipa_transcription = ''
-        target = word.lower()
-        entry = self.__get_entry(target)
+        entry = self._get_entry(word)
         if not entry:
-            target = self.__sanitize_word(target)
-            entry = self.__get_entry(target)
+            entry = self._get_entry(self._sanitize_word(word))
         if not entry:
-            ipa_transcription, matches_original = self.__compound_word_check(word.lower())
+            ipa_transcription, matches_original = self._compound_word_check(word)
             if not matches_original: 
-                sanitized_ipa_transcription, sanitized_matches_original = self.__compound_word_check(self.__sanitize_word(word.lower()))
+                sanitized_ipa_transcription, sanitized_matches_original = self._compound_word_check(self._sanitize_word(word))
                 if sanitized_matches_original:
                     ipa_transcription = sanitized_ipa_transcription
                 else:
                     ipa_transcription = ipa_transcription if len(ipa_transcription) > len(sanitized_ipa_transcription) else sanitized_ipa_transcription 
 
-        ipa_transcription = ipa_transcription or self.__get_ipa(entry)
-        return '\u0000' + ipa_transcription + '\u0000'  # Null character to mark the start and end of the IPA-string
+        ipa_transcription = ipa_transcription or self._get_ipa(entry) or None
+        return ipa_transcription
 
-    def __compound_word_check(self, word):
+    def _compound_word_check(self, word):
         substring = ''
         substring_ipa = ''
         while len(substring) != len(word):
             unchanged = True
             for i in range(len(word), len(substring), -1):
-                entry = self.__get_entry(word[len(substring):i])
+                entry = self._get_entry(word[len(substring):i])
                 if entry:
                     substring += word[len(substring):i]
-                    substring_ipa += self.__get_ipa(entry) or ''
+                    substring_ipa += self._get_ipa(entry) or ''
                     unchanged = False
                     break
             if unchanged:
                 break
         return substring_ipa, word == substring
 
-    def __get_entry(self, word):
-        if isinstance(word, str):
-            return self.lookup_table.get(word)
+    def _get_entry(self, word, pos):
+        if not word: return ''
+        assert(isinstance(word, str))
+
+        entry = self.lookup_table.get(word)  # Try normal word lookup
+        if not entry:
+            new_target = word.lower()
+            entry = self.lookup_table.get(new_target)  # Try all lower-case word
+        if not entry:
+            new_target = new_target.capitalize()
+            entry = self.lookup_table.get(new_target)  # Try captitalized word
+        if not entry:
+            new_target = new_target.upper()
+            entry = self.lookup_table.get(new_target)  # Try all caps word
+        return entry
         
     @staticmethod
-    def __sanitize_word(word):
+    def _sanitize_word(word):
         return re.sub('[^A-Za-z0-9üäöß ]+', '', word)
     
     @staticmethod
-    def __get_ipa(entry):
+    def _get_ipa(entry):
         if entry.get('ipa'):
             return entry.get('ipa')
         
